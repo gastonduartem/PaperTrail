ca1e8a4008a0d17bcddeccc412bd964939c382db
Sebastian Mujica <sebas13mujica@gmail.com>
2025-06-28 14:49:24 -0300

    Se agregaron comentarios

--- generacion_de_resumen_openAI.py
+++ generacion_de_resumen_openAI.py
@@ -4,26 +4,35 @@ import os
 import re
 import PyPDF2 
 load_dotenv()
+#Se consigue la api key del archivo .env
 api_key_ = os.getenv("OPENAI_API_KEY")
-
+#Se crea el objeto cliente y se configura la IA
 client = OpenAI(
     base_url="https://api.novita.ai/v3/openai",
-    api_key= api_key_,
+    api_key= api_key_
 )
-
+#Se selecciona el modelo de IA
 model = "deepseek/deepseek-r1"
+#Stream en False permite que la respuesta no aparezca por partes
 stream = False  # or False
+#Se limitan los token a usar
 max_tokens = 900
+
 def respuesta_ia(ruta_pdf):
     global texto
+    #Variable donde se guardara el texto extraido del pdf
     texto = ""
+    # Abrimos el archivo PDF en modo lectura binaria ("rb")
     with open(ruta_pdf, "rb") as archivo:
+        # Creamos un lector de PDF usando la librería PyPDF2
         lector = PyPDF2.PdfReader(archivo)
+        # Recorremos cada página del PDF
         for pagina in lector.pages:
-            texto += pagina.extract_text() + "\n"
-
+            # Extraemos el texto de la página y lo agregamos a la variable 'texto'
+            texto += pagina.extract_text() + "\n" # El "\n" agrega un salto de línea entre páginas
+    #Se guarda el texto en la variable a enviarse a la IA
     texto_pdf = texto 
-
+    #Se crea el obejeto que permite la comunicacion con la IA
     chat_completion_res = client.chat.completions.create(
         model=model,
         messages=[
@@ -31,7 +40,7 @@ def respuesta_ia(ruta_pdf):
                 "role": "system",
                 "content": "you are a professional AI helper。",
             },
-            {
+            {  #El esquema que usamos como usuaria para comunicarnos con la IA
                 "role": "user",
                 "content": f"""Como un experto en resúmenes de documentos, por favor, realiza un resumen conciso del archivo PDF que te proporcionaré.
                               Instrucciones:
@@ -51,9 +60,8 @@ def respuesta_ia(ruta_pdf):
             print(chunk.choices[0].delta.content or "", end="")
     else:
         respuesta = chat_completion_res.choices[0].message.content
+        #Se limpia la respuesta de la IA para tener no tener textos indeseados en la respuesta final
         respuesta_limpia = re.sub(r"<think>.*?</think>", "", respuesta, flags=re.DOTALL).strip()
   
     return respuesta_limpia 
 
-rp =respuesta_ia("documento.pdf")
-print(rp)
