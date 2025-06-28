dc574d9f0d391eadc7892f2821517604f4850e9b
Sebastian Mujica <sebas13mujica@gmail.com>
2025-06-28 12:49:56 -0300

    Solucion de errores y prueba de la respuesta de la IA

--- generacion_de_resumen_openAI.py
+++ generacion_de_resumen_openAI.py
@@ -1,23 +1,40 @@
 from openai import OpenAI
-import os
 from dotenv import load_dotenv
-
-
+import os
+import re
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key_ = os.getenv("OPENAI_API_KEY")
 
-client = OpenAI(api_key=api_key,base_url="https://api.novita.ai/v3/openai")
-historial = [{"role":"system","content":""}]
-def Open_IA(user_promt):
-  historial.append({"role":"user","content":user_promt}) #Agregamos el formato del mensaje con el prompt del user al historial 
-  chat= client.chat.completions.create(
-      model= "deepseek/deepseek-r1",
-      messages=historial,
-      stream= False,
-      temperature= 0.7
-  )   
-  ia_response = chat.choices[0].message.content #guardamos la respuesta 
-  # historial.append({"role":"assistant","content": ia_response}) #agregamos la respuesta al historial para que la IA recuerde la conversacion
-  return ia_response
+client = OpenAI(
+    base_url="https://api.novita.ai/v3/openai",
+    api_key= api_key_,
+)
 
-Open_IA("Hola como estas?")
+model = "deepseek/deepseek-r1"
+stream = False  # or False
+max_tokens = 512
+
+chat_completion_res = client.chat.completions.create(
+    model=model,
+    messages=[
+        {
+            "role": "system",
+            "content": "you are a professional AI helper。",
+        },
+        {
+            "role": "user",
+            "content": "Hello, How are you",
+        }
+    ],
+    stream=stream,
+    max_tokens=max_tokens,
+)
+
+if stream:
+    for chunk in chat_completion_res:
+        print(chunk.choices[0].delta.content or "", end="")
+else:
+    respuesta = chat_completion_res.choices[0].message.content
+    respuesta_limpia = re.sub(r"<think>.*?</think>", "", respuesta, flags=re.DOTALL).strip()
+  
+print(respuesta_limpia)
