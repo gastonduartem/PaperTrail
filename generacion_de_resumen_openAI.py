4c004ce1813a623779a2105da59d0df7d5c3e5da
Sebastian Mujica <sebas13mujica@gmail.com>
2025-06-28 14:24:00 -0300

    improvement

--- generacion_de_resumen_openAI.py
+++ generacion_de_resumen_openAI.py
@@ -14,37 +14,37 @@ client = OpenAI(
 model = "deepseek/deepseek-r1"
 stream = False  # or False
 max_tokens = 512
-def extraer_texto_pdf(ruta_pdf):
+def respuesta_ia(ruta_pdf):
+    global texto
     texto = ""
     with open(ruta_pdf, "rb") as archivo:
         lector = PyPDF2.PdfReader(archivo)
         for pagina in lector.pages:
             texto += pagina.extract_text() + "\n"
-    return texto
 
-texto_pdf = extraer_texto_pdf("documento.pdf")
+    texto_pdf = texto 
 
-chat_completion_res = client.chat.completions.create(
-    model=model,
-    messages=[
-        {
-            "role": "system",
-            "content": "you are a professional AI helper。",
-        },
-        {
-            "role": "user",
-            "content": f"Puedes resumir brevemente este archivo pdf, en un maximo de 3 parrafos y en terminos sensillos: \n{texto_pdf}",
-        }
-    ],
-    stream=stream,
-    max_tokens=max_tokens,
-)
+    chat_completion_res = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": "system",
+                "content": "you are a professional AI helper。",
+            },
+            {
+                "role": "user",
+                "content": f"Puedes resumir brevemente este archivo pdf, en un maximo de 3 parrafos y en terminos sensillos: \n{texto_pdf}",
+            }
+        ],
+        stream=stream,
+        max_tokens=max_tokens,
+    )
 
-if stream:
-    for chunk in chat_completion_res:
-        print(chunk.choices[0].delta.content or "", end="")
-else:
-    respuesta = chat_completion_res.choices[0].message.content
-    respuesta_limpia = re.sub(r"<think>.*?</think>", "", respuesta, flags=re.DOTALL).strip()
+    if stream:
+        for chunk in chat_completion_res:
+            print(chunk.choices[0].delta.content or "", end="")
+    else:
+        respuesta = chat_completion_res.choices[0].message.content
+        respuesta_limpia = re.sub(r"<think>.*?</think>", "", respuesta, flags=re.DOTALL).strip()
   
-print(respuesta_limpia)
+    return respuesta_limpia 
